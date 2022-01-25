b13daf88cbccb3f7d09b4d0c844e380c256c3d04
Saad332556 <332556@student.mboutrecht.nl>
2022-01-25 13:52:21 +0100

    last few things
    added: few things, events, over ons.
    changed: few things
    delete: double bg pic

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
                 <a href="#events">Evenementen</a>
                 <a href="#inschrijven">Inschrijven</a>
                 <a href="#contact">Contact</a>
-                <a href="read.php">Aangemelden?</a>
+                <a href="read.php">Login</a>
                 <a href="javascript:void(0);" class="icon" onclick="myFunction()">
                 <i class="fa fa-bars"></i></a>
             </ul>
@@ -50,57 +50,82 @@
             </div>
         </section>
         <section id="overons">
-            <div class="overons container">
-                <div class="overons-top">
-                    <!--Titel Pagina-->
-                    <h1 class="section-title">OVER ONS</h1>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                    At quam dolores eligendi qui doloremque nam animi distinctio delectus, 
-                    obcaecati mollitia, nihil et sunt perferendis itaque. 
-                    Tempora ab eos voluptates perferendis.</p>
-                </div>
-                <div class="overons-bottom">
-                    <div class="overons-item">
-                        <!--Icoontjes voor projecten-->
-                        <div class="icon">
-                        <img src="img/image.png" alt="">
+            <div class="container1">
+                <div class="content-section">
+                    <div class="title1">
+                        <h1>Over Ons</h1>
                     </div>
-                    <!--Projecten-->
-                    <h2>Project1</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                    At quam dolores eligendi qui doloremque nam animi distinctio delectus, 
-                    obcaecati mollitia, nihil et sunt perferendis itaque. 
-                    Tempora ab eos voluptates perferendis.</p>
-                </div>
-                <div class="overons-item">
-                    <div class="icon">
-                        <img src="img/image.png" alt="">
+                    <div class="content">
+                        <h3>Wij organiseren de beste evenementen in heel Nederland en België</h3>
+                        <p>Wij hebben zo al een aardig groot stuk van de game-community in Nederland & België van events voorzien.
+                            <br>
+                           Voor meer informatie over onze evenementen, verwijzen wij u graag door naar onze Evenementen pagina, via het knopje hieronder!</p>
+                        <div class="button">
+                            <a href="#events">Evenementen</a>
+                        </div>
                     </div>
-                    <h2>Project2</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                    At quam dolores eligendi qui doloremque nam animi distinctio delectus, 
-                    obcaecati mollitia, nihil et sunt perferendis itaque. 
-                    Tempora ab eos voluptates perferendis.</p>
                 </div>
-                <div class="overons-item">
-                    <div class="icon">
-                        <img src="img/image.png" alt="">
-                    </div>
-                    <h2>Project3</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                    At quam dolores eligendi qui doloremque nam animi distinctio delectus, 
-                    obcaecati mollitia, nihil et sunt perferendis itaque. 
-                    Tempora ab eos voluptates perferendis.</p>
+                <div class="image-section">
+                    <img src="img/over_ons.jpg" alt="">
                 </div>
-                <div class="overons-item">
-                    <div class="icon">
-                        <img src="img/image.png" alt="">
-                    </div>
-                    <h2>Project4</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                    At quam dolores eligendi qui doloremque nam animi distinctio delectus, 
-                    obcaecati mollitia, nihil et sunt perferendis itaque. 
-                    Tempora ab eos voluptates perferendis.</p>
+            </div>
+            <div class="icons">
+                <div class="prijzengeld">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="55" height="50" fill="currentColor" class="bi bi-currency-euro" viewBox="0 0 16 16">
+                        <path d="M4 9.42h1.063C5.4 12.323 7.317 14 10.34 14c.622 0 1.167-.068 1.659-.185v-1.3c-.484.119-1.045.17-1.659.17-2.1 0-3.455-1.198-3.775-3.264h4.017v-.928H6.497v-.936c0-.11 0-.219.008-.329h4.078v-.927H6.618c.388-1.898 1.719-2.985 3.723-2.985.614 0 1.175.05 1.659.177V2.194A6.617 6.617 0 0 0 10.341 2c-2.928 0-4.82 1.569-5.244 4.3H4v.928h1.01v1.265H4v.928z"/>
+                      </svg>
+                      <h3>250K</h3>
+                      <h4><b>PRIJZENGELD UITGEKEERD</b></h4>
+                </div>
+                <div class="trophies">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="55" height="50" fill="currentColor" class="bi bi-trophy" viewBox="0 0 16 16">
+                        <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935zM3.504 1c.007.517.026 1.006.056 1.469.13 2.028.457 3.546.87 4.667C5.294 9.48 6.484 10 7 10a.5.5 0 0 1 .5.5v2.61a1 1 0 0 1-.757.97l-1.426.356a.5.5 0 0 0-.179.085L4.5 15h7l-.638-.479a.501.501 0 0 0-.18-.085l-1.425-.356a1 1 0 0 1-.757-.97V10.5A.5.5 0 0 1 9 10c.516 0 1.706-.52 2.57-2.864.413-1.12.74-2.64.87-4.667.03-.463.049-.952.056-1.469H3.504z"/>
+                      </svg>
+                      <h3>40</h3>
+                      <h4><b>ONLINE CUPS</b></h4>
+                </div>
+                <div class="calendar">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="55" height="50" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
+                        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
+                      </svg>
+                      <h3>20</h3>
+                      <h4><b>LIVE EVENTS</b></h4>
+                </div>
+                <div class="deelnemers">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="55" height="50" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                      </svg>
+                      <h3>500K</h3>
+                      <h4><b>DEELNEMERS</b></h4>
+                </div>
+            </div>
+        </section>
+        <section id="events">
+            <h1 id="titel2">Evenementen</h1>
+            <h3 class="text1">
+                Hieronder staan onze top 3 evenementen die wij hebben georganiseerd.
+            </h3>
+            <h3>
+                Klik gerust op 1 van de 3 afbeeldingen om je aan te melden bij de volgende event.
+            </h3>
+            <div class="container2">
+                <div class="column1">
+                    <a href="#form"><img src="img/cod_event.jpg" style="width: auto; height: 150px;"></a>
+                    <h3>Call of Duty: Black Ops (Activision)</h3>
+                    <p class="e_1">Onze grootste en meest bekendste event. </p>
+                    <p>Bij deze event hadden we een deelnemers count van al maar liefst 150.000. Het was één van de drukste maar leukste eventen, en blijkbaar waren wij niet de enigsten die dat vonden hadden vele fans ons laten weten.</p>
+                </div>
+                <div class="column2">
+                    <a href="#form"><img src="img/fifa_event.jpg" style="width: auto; height: 150px;"></a>
+                    <h3>FIFA 2021 (EA)</h3>
+                    <p class="e_2">Onze op één na grooste event.</p>
+                    <p>Bij deze event hadden we een deelnemers count van 120.500, dat het op de 2e meest bekendste events van ons maakt.</p>
+                </div>
+                <div class="column3">
+                    <a href="#form"><img src="img/fortnite_event.jpeg" style="width: auto; height: 150px;"></a>
+                    <h3>Fortnite Esport (Activision)</h3>
+                    <p class="e_3">Onze derde meest grootste event.</p>
+                    <p>Het stond heel dichtbij bij onze FIFA 2021 event, met een deelnemers count van 110.000.</p>
                 </div>
             </div>
         </section>
@@ -114,9 +139,9 @@
                         wie weet zien we jou binnenkort bij één van onze evenementen!
                     </h3>
                 </div>
-                <div class="container">
-                    <div class="row1">
-                        <div class="column1">
+                <div class="container3">
+                    <div class="row3">
+                        <div class="column8">
                             <form action="create.php" method="post">
                                 <div class="row">
                                     <div class="col-25">
@@ -173,11 +198,11 @@
                                     </div>
                                 </div>
                                 <div class="row">
-                                    <input type="submit" value="Submit">
+                                    <input type="submit" value="Verstuur">
                                 </div>
                             </form>
                         </div>
-                        <div class="column2" id="contact">
+                        <div class="column9" id="contact">
                             <p class="contact2">
                                 Heb jij nog andere vragen of wil je liever met één van ons direct spreken of chatten? Klik dan op één van de onderstaande links.
                             </p>
@@ -186,7 +211,7 @@
                                 Telefoon receptie: <a class="link" href="tel:0031302469241">030 - 24 69 241</a><br>
                                 Email: <a class="link" href="mailto:info@optimalgaming.nl" target="_blank" rel="noopener">info@optimalgaming.nl</a>
                             </p>
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2451.5931112709663!2d5.158462715679926!3d52.08713647973455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c668a2918b16e9%3A0x95fac93d2c242c97!2sDaltonlaan%20300%2C%203584%20BK%20Utrecht!5e0!3m2!1snl!2snl!4v1635882379171!5m2!1snl!2snl" style="width: 100%; width: 480px; height: 300px; border:0; text-align: center;" allowfullscreen="" loading="lazy"></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2451.5931112709663!2d5.158462715679926!3d52.08713647973455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c668a2918b16e9%3A0x95fac93d2c242c97!2sDaltonlaan%20300%2C%203584%20BK%20Utrecht!5e0!3m2!1snl!2snl!4v1635882379171!5m2!1snl!2snl" style="width: 100%; height: 270px; border:0; text-align: center;" allowfullscreen="" loading="lazy"></iframe>
                         </div>
                     </div>
                 </div>
